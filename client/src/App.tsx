6ecf6e6310247a6a76c01b3929265f2576ead707
Jan-Krystof Kujel Zahradnik <jan.zahradnik@corellium.com>
2026-02-19 14:05:13 +0100

    feat: add react router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,80 +1,15 @@
-import { useEffect, useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Home from "./pages/home";
 import "./App.css";
-import SignUp from "./pages/signup";
-import Login from "./pages/login";
-import { client } from "./lib/api-client";
-import { getClientAuth } from "./lib/auth-client-checks";
-import { authClient } from "./lib/auth-client";
-import type { Account } from "better-auth";
 
-type Item = {
-  id: string;
-  name: string;
-  description: string;
-  link: string;
-  photoLink: string;
-};
-
-type Category = {
-  name: string;
-};
-
-function App() {
-  const [items, setItems] = useState<Item[] | null>(null);
-
-  const [category, setCategory] = useState<Category[] | null>(null);
-  const [jwt, setJwt] = useState<string | undefined>(undefined);
-  const [acc, setAcc] = useState<any | undefined>(undefined);
-
-  useEffect(() => {
-    async function getItems() {
-      const { data } = await client.request<Item[]>("/items");
-      if (data) {
-        setItems(data);
-      }
-    }
-    getItems();
-  }, []);
-
-  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
-
-  useEffect(() => {
-    async function checkAuth() {
-      const { user, isAuthenticated: authenticated } = await getClientAuth();
-      const t = (await authClient.token()).data?.token;
-      setJwt(t);
-      setIsAuthenticated(authenticated);
-      setAcc(user);
-    }
-    checkAuth();
-  }, []);
-
-  useEffect(() => {
-    async function getCategories() {
-      const { data } = await client.request<Category[]>("/categories");
-      if (data) {
-        setCategory(data);
-      }
-    }
-    getCategories();
-  }, []);
-
+export default function App() {
   return (
-    <div>
-      <pre>{items && JSON.stringify(items, null, 4)}</pre>
-      <h1>login</h1>
-      {category && <pre>{category && JSON.stringify(category, null, 4)}</pre>}
-
-      {isAuthenticated &&
-        JSON.stringify({ user: acc, token: jwt, valid: true })}
-      <Login
-        isAuthenticated={isAuthenticated}
-        setIsAuthenticated={setIsAuthenticated}
-      />
-      <h1>signup</h1>
-      <SignUp />
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
-
-export default App;
